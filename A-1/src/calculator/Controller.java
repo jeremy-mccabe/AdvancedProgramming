374e833384fe380eaf5d99f4f961bd498d26ddbf
jeremy <jeremy.a.mccabe@gmail.com>
2021-09-05 13:18:04 -0700

    Initial commit

--- A-1/src/calculator/Controller.java
+++ A-1/src/calculator/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package calculator;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
--- A-1/src/calculator/Main.java
+++ A-1/src/calculator/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
